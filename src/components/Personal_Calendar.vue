34b3b0a6c60a86bbb45493d31798c0ca22604ae4
Matt O <mo121@duke.edu>
2018-04-22 20:58:07 -0400

    Personal Calendar Component finished

---
+++ src/components/Personal_Calendar.vue
@@ -0,0 +1,45 @@
+<template>
+  <!-- template must have a SINGLE root tag that encloses all others -->
+  <table>
+    <tr v-for="(one, index1) in test">
+      Day {{index1+1}}
+      <th v-for="(two, index2) in one" v-bind:style="[two ? trueStyle : falseStyle]">
+
+      </th>
+    </tr>
+    <!--<p>{{test[0]}}</p>-->
+  </table>
+</template>
+
+<script>
+  // export anonymous object from this module so it can be accessed by others when imported
+  export default {
+    name: 'Personal_Calendar',
+    props: [ 'test'],
+    data: function() {
+      return {
+        thing: "Hi",
+        trueStyle: {
+          backgroundColor: 'green'
+        },
+        falseStyle: {
+          backgroundColor: 'red'
+        }
+      }
+    }
+
+  }
+</script>
+
+<style lang="scss">
+  table tr th {
+    border: 1px solid black;
+    width: 50px;
+  }
+  .green {
+    background-color: green;
+
+  }
+</style>
+
+
