3a6bd3509353fe7ed35076d10b848dff1d8f1232
Matt O <mo121@duke.edu>
2018-04-23 13:22:01 -0400

    Personal calendar

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,34 @@
 <template>
   <div id="app">
+    <div v-for="team in teams">
+      <p>{{team}}</p>
+    </div>
+    <h1>Personal Calendar:</h1>
     <Personal_Calendar :test="calendarInfo"></Personal_Calendar>
   </div>
 </template>
 
 <script>
+  import Firebase from 'firebase';
 
   import Personal_Calendar from './components/Personal_Calendar.vue'
+
+  var config = {
+    apiKey: "AIzaSyCk3ttnDL-mfdMNJO27thtvd31CvRxpmvM",
+    authDomain: "final-efcc8.firebaseapp.com",
+    databaseURL: "https://final-efcc8.firebaseio.com",
+    projectId: "final-efcc8",
+    storageBucket: "final-efcc8.appspot.com",
+    messagingSenderId: "718555914790"
+  };
+
+  var db = Firebase.initializeApp(config).database();
+  var teamsRef = db.ref('Teams');
 
 export default {
   name: 'app',
   data () {
+    console.log(this.books);
     return {
       calendarInfo: [
         [true, true, true, false, false, false, false, true],
@@ -19,7 +37,13 @@ export default {
         [false, true, true, true, false, false, false, true],
         [false, false, false, true, false, false, false, false]
       ],
-      msg: 'This is a cool calendar!'
+      msg: 'This is a cool calendar!',
+      teams: [1,2,3,4,5]
+    }
+  },
+  firebase() {
+    return {
+      teams: teamsRef
     }
   },
   components: {
