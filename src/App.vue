2250958a5c03457f90a8381dc2a82c0b3f2d9327
Matt O <mo121@duke.edu>
2018-04-30 01:11:24 -0400

    .

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
         <div class="col-lg-4">
             <div class="nav" id="openBtn">☰</div>
             <div class="sidenav">
+              <p>Test</p>
                 <a href="javascript:void(0)" class="closebtn" id="closeBtn">&times;</a>
 
                 <tr class="navOption">
@@ -229,7 +230,7 @@
             $('body').css('margin-left', '0');
         });
     });
-    
+
 </script>
 
 <style lang="scss">
@@ -293,7 +294,7 @@
     .sidenav {
         background-color: white;
         height: 100%; width: 0;
-        position: fixed; z-index: 1;
+        position: fixed;
         top: 0; left: 0;
         overflow: hidden;
         padding-top: 10%;
